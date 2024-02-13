f7a4df0f8617a1f8a9ee379ecbb796bd99799462
Seva <vsevolodharlamenko@gmail.com>
2024-02-13 21:47:32 +0400

    Added some variables Lesson_1:Task_1

--- Lesson_1/Task_1/Program.cs
+++ Lesson_1/Task_1/Program.cs
@@ -1,5 +1,13 @@
-﻿Console.WriteLine("Hello World!");
+﻿// Console.WriteLine("Hello World!");
 
 int num = 77;
 
 Console.WriteLine(num);
+
+double x = 7.77;
+char y = '!';
+bool z = true;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
