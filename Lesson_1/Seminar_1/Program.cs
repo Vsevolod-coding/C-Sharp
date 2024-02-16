4ea832f76d29004ecf358e7cc28270afd6b43e47
Seva <vsevolodharlamenko@gmail.com>
2024-02-16 13:33:28 +0400

    Task_2 Done While For

--- Lesson_1/Seminar_1/Program.cs
+++ Lesson_1/Seminar_1/Program.cs
@@ -1,8 +1,33 @@
-﻿int first = 25;
-int second = 5;
+﻿// Задание 1
 
-if(second * second == first){
-    Console.WriteLine("Yes");
+//int first = 25;
+//int second = 5;
+
+//if(second * second == first){
+//    Console.WriteLine("Yes");
+//}
+//else{Console.WriteLine("No");
+//}
+
+// Задание 2
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+
+int int_num = 3;
+int min = -int_num;
+
+while(min <= int_num){
+    Console.WriteLine(min+" ");
+    min++;
 }
-else{Console.WriteLine("No");
+
+// То же самое только через for
+
+int int_num = 3;
+
+for(int min = -int_num; min <= int_num; min++){
+    Console.WriteLine(min+" ");
 }
