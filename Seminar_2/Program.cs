932624db83b223103bf4285fab39858aec63023b
Seva <vsevolodharlamenko@gmail.com>
2024-02-19 12:27:17 +0400

    Task 1 DoneС

---
+++ Seminar_2/Program.cs
@@ -0,0 +1,8 @@
+﻿int num = 457;
+int first = num / 100;
+int last = num % 10;
+
+int sum = (first * 10) + last;
+
+// Console.Write("Number without middle digit = "+first+last);
+Console.Write(sum);
