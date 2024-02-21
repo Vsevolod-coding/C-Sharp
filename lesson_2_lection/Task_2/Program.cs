3ceac6baa449b0b8919ab0a53ee8cf0323338d99
Seva <vsevolodharlamenko@gmail.com>
2024-02-21 22:18:11 +0400

    Task 2 Done

---
+++ lesson_2_lection/Task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] arr = { 66, 2, 8, 56, 76, 978, 72, 4, 62, 914 };
+int index = 0;
+
+while (index < arr.Length){
+    if (arr[index] % 2 == 0){
+        Console.Write($"{arr[index]} ");
+    }
+    index += 1;
+}
