dd925cbd865bc837c431de2b2e4a0e9bb5a17a2d
Seva <vsevolodharlamenko@gmail.com>
2024-02-21 21:26:27 +0400

    added some lists

---
+++ lesson_2_lection/Training/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[5];
+arr[0] = 3;
+arr[1] = 8;
+arr[2] = 4;
+arr[3] = 7;
+arr[4] = 2;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5] { 77, 9, 4, 5, 8 };
+
+int[] arr3 = { 2, 3, 6, 8, 5, 0, 9 }
