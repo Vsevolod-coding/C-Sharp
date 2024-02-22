2d4ade4905b9623677cbe455c16790c70fda65eb
Seva <vsevolodharlamenko@gmail.com>
2024-02-22 14:36:27 +0400

    Task_4 Done

---
+++ homework_sem2/Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4: Напишите программу, которая на вход
+// принимает натуральное число N, а на выходе
+// показывает его цифры через запятую.
+
+int num = 734657345;
+
+string digitsString = num.ToString();
+Console.WriteLine(digitsString.Length);
+for (int i = 0; i < digitsString.Length; i++)
+{
+    Console.Write(digitsString[i]);
+    if (i < digitsString.Length - 1)
+    {
+        Console.Write(", ");
+    }
+}
