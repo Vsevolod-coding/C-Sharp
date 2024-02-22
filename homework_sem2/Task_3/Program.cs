f816fd7aebcdb6004fde14d8a5c360672c24ca08
Seva <vsevolodharlamenko@gmail.com>
2024-02-22 14:07:34 +0400

    Task_3 Done

---
+++ homework_sem2/Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: Напишите программу, которая принимает
+// на вход целое число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+
+int num = 59;
+int first = num / 10;
+int last = num % 10;
+
+if (10 < num && num < 99){
+    if (first > last){
+        Console.WriteLine(first);
+    }
+    else{
+        Console.WriteLine(last);
+    }
+}
