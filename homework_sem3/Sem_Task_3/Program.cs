802ff0558be4e949df1cfed3b49da81a620c7d1c
Seva <vsevolodharlamenko@gmail.com>
2024-02-22 18:43:26 +0400

    task 3 done

---
+++ homework_sem3/Sem_Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Задайте массив из вещественных чисел с
+// ненулевой дробной частью. Найдите разницу между
+// максимальным и минимальным элементов массива. 
+
+double [] array = {1.273, 8.2345, 2.98, 0.3143, 1.34, 5.55};
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++){
+    if (array[i] > max){
+        max = array[i];
+    }
+    if (array[i] < min){
+        min = array[i];
+    }
+}
+Console.WriteLine("Min = " + min + " Max = " + max);
+Console.WriteLine("Разница между MAX и MIN элементов массива = " + (max - min));
