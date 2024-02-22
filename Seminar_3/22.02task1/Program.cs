666a151d55f618bf657252310a11a2b31efeb182
Seva <vsevolodharlamenko@gmail.com>
2024-02-22 13:21:17 +0400

    Seminar_3 new folder

---
+++ Seminar_3/22.02task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+
+int num = 100;
+int [] array = new int [] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+bool flag = false;
+
+foreach(int item in array){
+    if(item == num){
+        flag = true;
+    }
+}
+
+if(flag ){
+    Console.Write("ДА");
+}
+else{
+    Console.Write("Нет");
+}
