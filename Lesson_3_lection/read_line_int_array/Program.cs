1f6e8fe32445e1764aa49ba465f5834fe681afc1
Seva <vsevolodharlamenko@gmail.com>
2024-02-26 14:20:58 +0400

    Console.ReadLine(); input element to arrayо

---
+++ Lesson_3_lection/read_line_int_array/Program.cs
@@ -0,0 +1,22 @@
+﻿int size = 10;
+int[] array = new int[size];
+int i = 0;
+while (i < size)
+{
+    Console.WriteLine("Введите элемент массива (целове число): ");
+    string input = Console.ReadLine();
+    array[i] = Convert.ToInt32(input);
+    i++;
+}
+
+i = 0;
+Console.WriteLine("Ваш массив:");
+while (i < size)
+{
+    Console.Write($"{array[i]}");
+    if (i < array.Length - 1)
+    {
+        Console.Write(", ");
+    }
+    i++;
+}
