bb6c3bc656b440622ceac6443d595fdde4bb250d
Seva <vsevolodharlamenko@gmail.com>
2024-02-26 13:45:10 +0400

    Task3 Done ZeroEvenElements PrintArray

---
+++ Lesson_3_lection/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = [0, 0, 0, 2, 234, 534, 123, 8273645, 2345235, 234, 1111];
+
+ZeroEvenElements(array);
+PrintArray(array);
