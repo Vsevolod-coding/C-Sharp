35275698242674da0bcaf083c2e334eed4f19642
Seva <vsevolodharlamenko@gmail.com>
2024-02-26 12:45:39 +0400

    Task 1 done calc formula

---
+++ Lesson_3_lection/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numerator = a * b;
+    int denominator = c + d;
+    double result = numerator / denominator;
+    return result;
+}
+
+double result = CalculateFormula(8, 10, 5, 6);
+Console.WriteLine(result);
