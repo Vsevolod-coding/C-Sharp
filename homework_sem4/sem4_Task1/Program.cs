87ef4231b8a1f08b19797a5164e2961734d32271
Seva <vsevolodharlamenko@gmail.com>
2024-02-28 19:30:38 +0400

    Task 1 Done Сумма цифр числа

---
+++ homework_sem4/sem4_Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 1
+// Напишите программу, которая бесконечно запрашивает
+// целые числа с консоли. Программа завершается при вводе символа ‘q’
+// или при вводе числа, сумма цифр которого чётная.
+
+Console.Clear();
+while (true)
+{
+    Console.Write("Введите число: ");
+    string input = Console.ReadLine()!;
+
+    if (input.ToLower() == "q")
+    {
+        Console.WriteLine("Программа завершена.");
+        break;
+    }
+
+    if(IsSumOfDigitsEven(input)){
+        Console.WriteLine("Сумма цифр числа четная. Программа завершена.");
+        break;
+    }
+
+    bool IsSumOfDigitsEven(string sum)
+    {
+        int converted_num = Convert.ToInt32(sum);
+        int sum_digits = 0;
+        while (converted_num > 0)
+        {
+
+            sum_digits = sum_digits + converted_num % 10;
+            converted_num /= 10;
+        }
+            
+        return sum_digits % 2 == 0;
+    }
+};
+
+
+
