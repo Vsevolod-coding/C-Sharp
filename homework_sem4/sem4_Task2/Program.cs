b6df01ac0d8b583a7da96431d122c0d01a9bf152
Seva <vsevolodharlamenko@gmail.com>
2024-03-03 11:32:11 +0400

    Task 2 done evennums in array CountEvenNums(){}

---
+++ homework_sem4/sem4_Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] array = {213, 543, 986, 742, 898, 123, 499, 998, 777, 348, 568, 522, 742, 943, 871, 880, 222, 444};
+
+int CountEvenNums(int[] NumArr)
+{
+    int CountEven = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(NumArr[i] % 2 == 0)
+        {
+            CountEven++;
+        }
+    }
+    return CountEven;
+}
+
+int result = CountEvenNums(array);
+
+Console.Write($"В массиве {result} четных чисел.");
