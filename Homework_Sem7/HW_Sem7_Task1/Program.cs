d42b881bad76dc744222d54590cd5959412722dd
Vsevolod <158557439+Vsevolod-coding@users.noreply.github.com>
2024-03-09 21:55:14 +0400

    recursion Homework

---
+++ Homework_Sem7/HW_Sem7_Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1:
+// Задайте значения M и N.
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+int M = 7;
+int N = 17;
+
+
+void PrintNaturalNums(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(m + " ");
+        PrintNaturalNums(m + 1, n);
+    }
+}
+
+Console.WriteLine($"Натуральные числа в промежутке от {M} до {N}:");
+PrintNaturalNums(M, N);
---
+++ Homework_Sem7/HW_Sem7_Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+void ReversedArray(int[] array, int index)
+{
+    if(index >= 0)
+    {
+        Console.Write($"{array[index]} ");
+        ReversedArray(array, index-1);
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Not Reversed");
+
+int[] array = { 4, 2, 1, 0, 32, 85, 100 };
+// Print нач. массив
+foreach (int item in array)
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine("\nReversed");
+
+ReversedArray(array, array.Length -1);
